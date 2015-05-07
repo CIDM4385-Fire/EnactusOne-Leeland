3ca6732e131bf8b1f21c376c07950cf6fa6cc372
WestTexasAM <leeland_h@hotmail.com>
2015-05-07 09:40:33 -0500

    Projects is working!!!
    Got the projects to work

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -52,32 +52,17 @@ function doClick1() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 1',
 		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
-		});
 			var info=Titanium.UI.createButton({
 			title:'Project 1 Information Goes Here',
 			width: Ti.UI.SIZE,
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 function doClick2() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 2',
-		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
 		});
 			var info=Titanium.UI.createButton({
 			title:'Project 2 Information Goes Here',
@@ -85,19 +70,11 @@ function doClick2() {
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 function doClick3() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 3',
-		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
 		});
 			var info=Titanium.UI.createButton({
 			title:'Project 3 Information Goes Here',
@@ -105,19 +82,11 @@ function doClick3() {
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 function doClick4() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 4',
-		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
 		});
 			var info=Titanium.UI.createButton({
 			title:'Project 4 Information Goes Here',
@@ -125,19 +94,11 @@ function doClick4() {
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 function doClick5() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 5',
-		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
 		});
 			var info=Titanium.UI.createButton({
 			title:'Project 5 Information Goes Here',
@@ -145,19 +106,11 @@ function doClick5() {
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 function doClick6() {
 		var win = Titanium.UI.createWindow({
 			title: 'PROJECT 6',
-		});
-		var back=Titanium.UI.createButton({
-			title:'Back',
-			width: Ti.UI.SIZE,
-			height: Ti.UI.SIZE,
-			bottom : 10,
-			left : 10
 		});
 			var info=Titanium.UI.createButton({
 			title:'Project 6 Information Goes Here',
@@ -165,7 +118,6 @@ function doClick6() {
 			height: Ti.UI.SIZE
 		});
 		win.add(info);
-		win.add(back);
 		win.open();
 };
 $.projects.open();
